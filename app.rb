bf1913fd4d2aabcd988008a9f69f8950618ac9fe
mall324 <m_kunam@outlook.com>
2016-10-20 17:52:37 +0530

    Update app.rb

--- app.rb
+++ app.rb
@@ -3,6 +3,54 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require './environments'
+require "omniauth"
+require "omniauth-salesforce"
+
+configure do
+  enable :logging
+  enable :sessions
+  set :show_exceptions, false
+  set :session_secret, ENV['SECRET']
+end
+
+use OmniAuth::Builder do
+  provider :salesforce, ENV['SALESFORCE_KEY'], ENV['SALESFORCE_SECRET']
+end
+
+get '/auth/salesforce/callback' do
+  logger.info "#{env["omniauth.auth"]["extra"]["display_name"]} just authenticated"
+  credentials = env["omniauth.auth"]["credentials"]
+  session['token'] = credentials["token"]
+  session['refresh_token'] = credentials["refresh_token"]
+  session['instance_url'] = credentials["instance_url"]
+  redirect '/'
+end
+
+helpers do
+  def client
+    @client ||= Force.new instance_url:  session['instance_url'],
+                          oauth_token:   session['token'],
+                          refresh_token: session['refresh_token'],
+                          client_id:     ENV['SALESFORCE_KEY'],
+                          client_secret: ENV['SALESFORCE_SECRET']
+  end
+end
+
+get '/' do
+  logger.info "Visited home page"
+  @accounts= client.query("select Id, Name from Account")
+  erb :index
+end
+
+<table>
+<tr><th>Account</th><th>ID</th></tr>
+<% @accounts.each do |account| %>
+  <tr>
+     <td><%= account.Name %></td>
+     <td><%= account.Id %></td>
+  </tr>
+<% end %>
+</table>
 
 class Contact < ActiveRecord::Base
   self.table_name = 'salesforce.contact'
